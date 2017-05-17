c355c38b970c6b543baba37062fcebd88ede719c
Andy Kirkham <andy.kirkham@logicnow.com>
2017-05-17 16:34:50 +0100

    reactor_ssl.c spike is wip and basics work

--- spikes/reactor_ssl.c
+++ spikes/reactor_ssl.c
@@ -22,6 +22,8 @@
 
 // As spike project reactor.c but with SSL
 
+// Ref https://www.ibm.com/support/knowledgecenter/en/SSB23S_1.1.0.12/gtps7/s5sple1.html
+
 #include <stdio.h>
 
 #include <stdlib.h>
@@ -29,6 +31,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #include "../src/reactor.h"
 #include "../src/listener.h"
@@ -49,12 +52,29 @@ my_callback(const reactor_cb_args_pt inp_args);
 int
 main(int argc, char *argv[])
 {
-	int loop_counter = 0;
+	int rc, loop_counter = 0;
 	my_globals_t globals;
 
-	globals.p_sslctx = SSL_CTX_new(TLSv1_1_method());
+	SSL_load_error_strings();
+	SSL_library_init();
+	OpenSSL_add_all_algorithms();
 
-	globals.p_listener = listener_ctor("0.0.0.0", 8081);
+	globals.p_sslctx = SSL_CTX_new(TLSv1_1_method());
+	SSL_CTX_set_options(globals.p_sslctx, SSL_OP_SINGLE_DH_USE);	
+	rc = SSL_CTX_use_certificate_file(globals.p_sslctx, 
+		"/certs/localhost_ajk_io/localhost.ajk.io.ha.pem" , SSL_FILETYPE_PEM);
+	if(rc != 1) {
+		printf("Error on SSL_CTX_use_certificate_file()\n");
+		return 0;
+	}
+	rc = SSL_CTX_use_PrivateKey_file(globals.p_sslctx,
+		"/certs/localhost_ajk_io/localhost.ajk.io.key", SSL_FILETYPE_PEM);
+	if(rc != 1) {
+		printf("Error on SSL_CTX_use_PrivateKey_file()\n");
+		return 0;
+	}
+
+	globals.p_listener = listener_ctor("0.0.0.0", 443);
 	if(!globals.p_listener) {
 		printf("Failed ctor()\n");
 		return 0;
@@ -80,7 +100,8 @@ main(int argc, char *argv[])
 	listener_listen(globals.p_listener);
 
 	// Basic event loop
-	while(loop_counter < 10) {
+	printf("Entering event loop\n");
+	while(loop_counter < 30) {
 		reactor_loop_once_for(globals.p_reactor, 1000);
 		loop_counter++;
 	}
@@ -108,24 +129,40 @@ my_callback_accept(const reactor_cb_args_pt inp_args)
 {
 	my_data_pt p_data = calloc(1, sizeof(my_data_t));
 	if(p_data) {
+		int rc;
+		printf("my_callback_accept()...");
 		my_globals_t *p_globals = (my_globals_t*)inp_args->data.accept_args.p_userdata;
 		SSL_CTX *p_sslctx = p_globals->p_sslctx;
 		if(!p_sslctx) {
+			printf(" fail1\n");
 			close(inp_args->data.accept_args.accecpt_fd);
 			return -1;
 		}
 		p_data->fd = inp_args->data.accept_args.accecpt_fd;
 		p_data->addr_len = inp_args->data.accept_args.in_len;
 		memcpy(&p_data->addr, inp_args->data.accept_args.p_addr, p_data->addr_len);
-		p_data->p_ssl = SSL_new(p_sslctx);
+		if((p_data->p_ssl = SSL_new(p_sslctx)) == NULL) {
+			printf(" fail3\n");
+			close(inp_args->data.accept_args.accecpt_fd);
+			return -1;
+		}
 		SSL_set_fd(p_data->p_ssl, p_data->fd);
-		SSL_accept(p_data->p_ssl);
+		if((rc = SSL_accept(p_data->p_ssl)) < 1) {
+			rc = SSL_get_error(p_data->p_ssl, rc);
+			if(rc == SSL_ERROR_WANT_READ) SSL_read(p_data->p_ssl, NULL, 0);
+			else {
+				printf(" fail2 %d\n", rc);
+				close(inp_args->data.accept_args.accecpt_fd);
+				return -1;
+			}
+		}
 		// incoming ->data.accept_args.p_userdata points to &globals set on the reactor.
 		// change here to set it to the p_userdata value at the connection level.
 		// Note, this doesn't change the reactor's p_userdata, it only sets the
 		// per incoming fd p_userdata value which you need when processing events
 		// later on in the my_callback_event() function.
 		inp_args->data.accept_args.p_userdata = p_data;
+		printf(" ok\n");
 		return 0;
 	}
 	close(inp_args->data.accept_args.accecpt_fd);
@@ -141,15 +178,27 @@ my_callback_event(const reactor_cb_args_pt inp_args)
 	memset(buf, 0, sizeof(buf));
 	printf(" event %d %08x\n", inp_args->data.event_args.accepted_fd, inp_args->data.event_args.event);
 	assert(inp_args->data.event_args.p_userdata != NULL);
-	if(inp_args->data.event_args.event & REACTOR_IN) {
+	if(1 || inp_args->data.event_args.event & REACTOR_IN) {
 		while((len = SSL_read(p_data->p_ssl, buf, sizeof(buf))) > 0) {
 			printf("%s", buf);
 			SSL_write(p_data->p_ssl, buf, len);
 			memset(buf, 0, sizeof(buf));
 		}
-		SSL_free(p_data->p_ssl);
-		close(p_data->fd);
-		free(p_data);
+	 	if(len < 0) {
+			int rc = SSL_get_error(p_data->p_ssl, len);
+			if(rc != SSL_ERROR_WANT_READ && rc != SSL_ERROR_WANT_WRITE) {
+				SSL_shutdown(p_data->p_ssl);
+				SSL_free(p_data->p_ssl);
+				close(p_data->fd);
+				free(p_data);
+			}
+		}	
+		if(len == 0) {
+			SSL_shutdown(p_data->p_ssl);
+			SSL_free(p_data->p_ssl);
+			close(p_data->fd);
+			free(p_data);
+		}
 		return 0;
 	}
 	return -1;
